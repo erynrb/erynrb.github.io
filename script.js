901277823a299d99543b55b32d5519775943713d
Eryn <eryn.richardsonbartha@gmail.com>
2025-05-02 12:28:50 -0400

    button

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
+    var buttonText = document.getElementById("sidebarButton");
     sidebar.classList.toggle("show");
+    if (buttonText.innerHTML === "☰") {
+        buttonText.innerHTML = "x";
+    }else{
+        buttonText.innerHTML = "☰";
+    }
 }
